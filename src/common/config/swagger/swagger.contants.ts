175fa98133fcd15a2bf4e09ad5cbe569d70dbd52
Trend20 <enockomondi305@gmail.com>
2024-10-20 01:27:18 +0300

    adding swagger configurations for api documentation

---
+++ src/common/config/swagger/swagger.contants.ts
@@ -0,0 +1,5 @@
+export const SWAGGER_API_ROOT = 'api/docs';
+export const SWAGGER_API_NAME = 'NestJS Blog API';
+export const SWAGGER_API_DESCRIPTION =
+  'This is a NestJS Version of a Comprehensive Blog API';
+export const SWAGGER_API_CURRENT_VERSION = '1.0';
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { setupSwagger } from './common/config/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.setGlobalPrefix('api');
+  setupSwagger(app);
+  const port = process.env.SERVER_PORT;
+  await app.listen(port);
 }
 bootstrap();

0331df2c80220d5e4afbd08e3bcdba70573be0ae
Trend20 <enockomondi305@gmail.com>
2024-10-21 17:52:38 +0300

    implementing the auth module

---
+++ src/modules/auth/dto/login.dto.ts
@@ -0,0 +1,12 @@
+import { IsEmail, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+
+export class LoginDto {
+  @ApiProperty()
+  @IsString()
+  username: string;
+
+  @ApiProperty()
+  @IsString()
+  password: string;
+}
